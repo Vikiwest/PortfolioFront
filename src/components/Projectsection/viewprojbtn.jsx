464a77dd1c61f72e7fbae817409265dc6e970086
Vikiwest <chidiolorunda@gmail.com>
2025-01-16 20:08:18 +0100

    Updated

--- src/components/Projectsection/viewprojbtn.jsx
+++ src/components/Projectsection/viewprojbtn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Viewprojbtn() {
   return (
     <div>
-     <button className='button-txt
+     <button className='button-txt hover:bg-brandyellow hover:text-white
       w-[150px] h-[43px] border rounded-full font-medium'>View Project</button>
     </div>
   )
--- src/components/navbar/navanchors.jsx
+++ src/components/navbar/navanchors.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom"; // Import Link
 function Navanchors({ isOpen, closeMenu }) {
   return (
     <div className={`anchortxt space-x-16 font-bold md:flex flex-col md:flex-row ${isOpen ? "block" : "hidden"} md:block absolute md:static bg-white md:bg-transparent w-full md:w-auto top-14 left-0 shadow-lg md:shadow-none z-20`}>
-      <Link to="/about" className="block ml-16 p-4 md:p-0 hover:bg-gray-200 md:hover:bg-transparent transition duration-200" onClick={closeMenu}>About</Link>
-      <Link to="/contact" className="block p-4 md:p-0 hover:bg-gray-200 md:hover:bg-transparent transition duration-200" onClick={closeMenu}>Contact</Link>
+      <Link to="/about" className="block ml-16 p-4 md:p-0 hover:text-brandyellow hover:color md:hover:bg-transparent transition duration-200" onClick={closeMenu}>About</Link>
+      <Link to="/contact" className="block p-4 md:p-0 hover:text-brandyellow md:hover:bg-transparent transition duration-200" onClick={closeMenu}>Contact</Link>
     </div>
   );
 }
