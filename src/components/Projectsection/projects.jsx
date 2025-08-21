a6a83ca4852bcc67fba6d5378a7f767d42bc865b
Olorunda Victory <chidiolorunda@gmail.com>
2025-08-21 20:12:42 +0100

    Updated Projects

--- src/components/Projectsection/projects.jsx
+++ src/components/Projectsection/projects.jsx
@@ -6,13 +6,62 @@ import ActiveCatering from "../../assets/ActiveCatering.png";
 import Tariq from "../../assets/99Fashion.png";
 import myPortfolio from "../../assets/PortfolioProject.png";
 import sabitok from "../../assets/sabitok.png";
-
+import EduManager from "../../assets/Edu-manager.png";
+import Myxellia from "../../assets/Myxellia.png";
 
 function Projects() {
   return (
     <div id="projects" className="mt-[205px]">
       <h2 className="proj-header font-bold text-5xl text-center ">Projects</h2>
       <div className="mt-5 h-[4px]  w-[100px] m-auto bg-brandyellow rounded-full" />
+
+      <div className="m-auto w-[90%] md:w-[70%] flex flex-wrap mt-20  rounded-lg ">
+        <div className="w-full md:w-[666px] h-auto bg-OffWhite shadow-lg  rounded-l-3xl pt-10 pl-9 pr-9 pb-4 space-y-3">
+          <h2 className="weather-app text-[32px] md:text-[40px] font-bold leading-[1.5]">
+            Myxellia Dashboard
+          </h2>
+          <p className="weather-app-desc text-grey text-[18px] leading-[px]">
+            This is a dashboard for monitoring business activities, platform
+            users and other business activities. I built this using Next.Js,
+            Tailwindcss, Typescript.
+          </p>
+          <div className="mt-8 mb-5">
+            <a href="https://myxellia-delta-livid.vercel.app/" target="_blank">
+              <Viewprojbtn />
+            </a>
+          </div>
+        </div>
+        <div className="w-full md:w-[666px] h-auto">
+          <img className="rounded-r-3xl    shadow-lg " src={Myxellia} alt="" />
+        </div>
+      </div>
+
+      <div className="m-auto w-[90%] md:w-[70%] flex flex-wrap mt-20   ">
+        <div className="w-full md:w-[666px] h-auto">
+          <img
+            className="rounded-l-3xl    shadow-lg "
+            src={EduManager}
+            alt=""
+          />
+        </div>
+
+        <div className="w-full md:w-[666px] h-auto bg-OffWhite shadow-lg  rounded-r-3xl pt-10 pl-9 pr-9 pb-4 space-y-3">
+          <h2 className="weather-app text-[32px] md:text-[40px] font-bold leading-[1.5]">
+            Edu Manager{" "}
+          </h2>
+          <p className="weather-app-desc text-grey text-[18px] leading-[px]">
+            This is an App for managing students activities, and also has a
+            student dashboard for students to checkout activities and other
+            necessary curicular activities.
+          </p>
+          <div className="mt-8 mb-5">
+            <a href="https://edu-manager-lyart.vercel.app/" target="_blank">
+              {" "}
+              <Viewprojbtn />
+            </a>
+          </div>
+        </div>
+      </div>
 
       <div className="m-auto w-[90%] md:w-[70%] flex flex-wrap mt-20  rounded-lg ">
         <div className="w-full md:w-[666px] h-auto bg-OffWhite shadow-lg  rounded-l-3xl pt-10 pl-9 pr-9 pb-4 space-y-3">
@@ -105,8 +154,10 @@ function Projects() {
             99Tariq Fashion
           </h2>
           <p className="weather-app-desc text-grey text-[18px] leading-[px]">
-            This is a simple fashion website for users to view displayed, available products for purchase. I built this web app using just Html
-            and Css. It is mobile responsive and adjusts to all screen sizes.
+            This is a simple fashion website for users to view displayed,
+            available products for purchase. I built this web app using just
+            Html and Css. It is mobile responsive and adjusts to all screen
+            sizes.
           </p>
           <div className="mt-8 mb-5">
             <a
@@ -167,13 +218,13 @@ function Projects() {
             user-submitted responses and generates context-aware suggested
             replies based on custom settings selected by the user (e.g. tone,
             intent, or style). It also features a text rephraser, allowing users
-            to refine or adjust messages before sending them to someone.{" "} I worked closely with the design and product team including a backend developer to build thi responsive user interface.  <br /> <br /> ©️Monsieur Cruise LTD
+            to refine or adjust messages before sending them to someone. I
+            worked closely with the design and product team including a backend
+            developer to build thi responsive user interface. <br /> <br />{" "}
+            ©️Monsieur Cruise LTD
           </p>
           <div className="mt-8 mb-5">
-            <a
-              href="https://www.sabitok.com"
-              target="_blank"
-            >
+            <a href="https://www.sabitok.com" target="_blank">
               <Viewprojbtn />
             </a>
           </div>
